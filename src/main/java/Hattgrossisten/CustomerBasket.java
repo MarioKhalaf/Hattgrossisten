792a076703e54d32dbbde4e5853328fcc47211a0
Corn1344 <fabianlrstad@yahoo.se>
2023-01-13 18:14:43 +0100

    Added tests for method makeOrder in class  CustomerBasket

--- src/main/java/Hattgrossisten/CustomerBasket.java
+++ src/main/java/Hattgrossisten/CustomerBasket.java
@@ -10,11 +10,29 @@ public class CustomerBasket {
     List<String> payMethods = new ArrayList<String>();
     List<String> shippingMethods = new ArrayList<String>();
 
+    public static void main(String[] args) {
+    }
+
     public void pay() {
         System.out.println("Makes the customer pay");
     }
 
-    public void makeOrder() {
-        System.out.println("Makes the order");
+    public String makeOrder(List<String> products, List<String> payMethods, List<String> shippingMethods, Integer x, Integer price)
+            throws IndexOutOfBoundsException {
+        this.products = products;
+        this.payMethods = payMethods;
+        this.shippingMethods = shippingMethods;
+        this.price = price;
+        List<String> order = new ArrayList<>();
+        try {
+            order.add(products.get(x));
+            order.add(payMethods.get(x));
+            order.add(shippingMethods.get(x));
+            return "Products: " + order.get(0) + " | Order Total: " + price + " | Payment method: " + order.get(1) + " | Shipping method: " + order.get(2);
+        } catch (IndexOutOfBoundsException exception) {
+        }
+        String errormsg = ("The order does not exist.");
+        return errormsg;
+        
     }
 }
