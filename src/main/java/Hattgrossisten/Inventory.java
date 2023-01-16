cbec37d6ccd66ed2e223e1ed49ac596222c5d461
rlinddo22 <rickard.lindqvist@yh.nackademin.se>
2023-01-16 19:07:45 +0100

    removed unused method

--- src/main/java/Hattgrossisten/Inventory.java
+++ src/main/java/Hattgrossisten/Inventory.java
@@ -6,19 +6,15 @@ public class Inventory {
 
     public HashMap<String, Integer> inventoryMap = new HashMap<>();
 
-    public HashMap<String, Integer> getInventory() {         // Return entire warehouse inventory
+    public HashMap<String, Integer> getInventory() { // Return entire warehouse inventory
         return inventoryMap;
     }
 
-    public int getProductQuant(String productName) {         //Returns quantity in inventory of a specific product
+    public int getProductQuant(String productName) { // Returns quantity in inventory of a specific product
         return inventoryMap.get(productName);
     }
 
-    public void addNewProduct(String productName, int productQuantity) {     //Add a new product to the inventory
+    public void addNewProduct(String productName, int productQuantity) { // Add a new product to the inventory
         inventoryMap.put(productName, productQuantity);
-    }
-
-    public void quantity() {
-        System.out.println("Shows the inventory of all the products");
     }
 }
