559f040c1c0f18d9e809846b4140664f8ab64956
Mario Khalaf <Mario.khalaf9810@gmail.com>
2023-01-16 11:13:29 +0100

    fixed methods & merge

--- src/main/java/Hattgrossisten/Hat.java
+++ src/main/java/Hattgrossisten/Hat.java
@@ -1,15 +1,39 @@
 package Hattgrossisten;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hat extends Product {
+  
+    public Hat(int id, String name, int price, String size, int grade, int quantity) {
+        super(id, name, price, size, grade, quantity);
 
-    Hat(String name, int price, String size, int grade, String category) {
-        super(name, price, size, grade, category);
+    }
 
+    public Hat() {
     }
 
     @Override
     public String toString() {
-        return "name: " + name + ", price: " + price + ", size: " + size + ", grade: " + grade + ", category: "
-                + category;
+        return "Name  " + name + ", Price: " + price + ", Size: " + size + ", Grade: " + grade + ", Quantity: "
+                + quantity;
     }
+
+    public List<Hat> addHats() {
+        Hat a = new Hat(1, "Black Hat ", 50, "M", 3, 1);
+        Hat b = new Hat(2, "Red Hat   ", 80, "L", 1, 8);
+        Hat c = new Hat(3, "Blue Hat  ", 60, "S", 5, 7);
+        Hat d = new Hat(4, "Yellow Hat", 90, "M", 4, 4);
+        Hat e = new Hat(5, "Green Hat ", 75, "S", 3, 0);
+        System.out.println(a.grade + b.grade + c.grade + d.grade + e.grade);
+        List<Hat> hatList = new ArrayList<>();
+        hatList.add(a);
+        hatList.add(b);
+        hatList.add(c);
+        hatList.add(d);
+        hatList.add(e);
+        return hatList;
+
+    }
+
 }
