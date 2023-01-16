7d35149c05d5f2518079430ee8c142277cc3163d
rlinddo22 <rickard.lindqvist@yh.nackademin.se>
2023-01-16 20:08:35 +0100

    added inventory tests

--- src/test/java/Hattgrossisten/InventoryTests.java
+++ src/test/java/Hattgrossisten/InventoryTests.java
@@ -1,12 +1,33 @@
 package Hattgrossisten;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
-import static org.hamcrest.Matchers.*;
 
 public class InventoryTests {
 
+    @Test
+    public void shouldUpdateProductQuantityOnValidInput() {
+        Hat testHat = new Hat(1, "Awesome Beanie", 100, "M", 4, 5);
+        Inventory testInventory = new Inventory();
+        testInventory.addNewProduct(testHat.name, 20);
+        assertEquals(true, testInventory.updateProductQuant(testHat.getName(), 5));
+        assertEquals(5, testInventory.getProductQuant(testHat.getName()));
+    }
+
+    @Test
+    void shouldNotUpdateProductQuantityOnInvalidQuantity() {
+        Hat testHat = new Hat(1, "Awesome Beanie", 100, "M", 4, 5);
+        Inventory testInventory = new Inventory();
+        testInventory.addNewProduct(testHat.name, 20);
+        assertEquals(false, testInventory.updateProductQuant(testHat.getName(), -2));
+        assertEquals(20, testInventory.getProductQuant(testHat.getName()));
+    }
+
+    @Test
+    void shouldFailOnUpdatingNonExistantProduct() {
+        Hat testHat = new Hat(1, "Awesome Beanie", 100, "M", 4, 5);
+        Inventory testInventory = new Inventory();
+        testInventory.addNewProduct(testHat.name, 20);
+        assertEquals(false, testInventory.updateProductQuant("Awful Beanie", 10));
+    }
 }
