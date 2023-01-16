c46592986c494e5dd7d8d46f8fc8bd1521025141
Rickard Lindvist <rickard.lindqvst@yh.nackademin.se>
2023-01-16 15:50:51 +0100

    added comment

--- src/main/java/Hattgrossisten/OrderHistory.java
+++ src/main/java/Hattgrossisten/OrderHistory.java
@@ -3,7 +3,7 @@ package Hattgrossisten;
 import java.util.List;
 import java.util.ArrayList;
 
-public class OrderHistory {
+public class OrderHistory {             //contains all the orders the store has handled
     public static List<Orders> storeOrderHistory = new ArrayList<>();
     public static int nextOrderId = 10000;
     
