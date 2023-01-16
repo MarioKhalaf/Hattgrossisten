1f2774da1f91f33ecf3e69a7850afeec77521191
Rickard Lindvist <rickard.lindqvst@yh.nackademin.se>
2023-01-16 10:27:10 +0100

    yup

--- src/main/java/Hattgrossisten/Product.java
+++ src/main/java/Hattgrossisten/Product.java
@@ -1,5 +1,9 @@
 package Hattgrossisten;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Math;
+
 public class Product implements ProductInterface {
 
     public String name;
@@ -7,8 +11,10 @@ public class Product implements ProductInterface {
     public String size;
     public int grade;
     public String category;
+    public List<Integer> gradeList = new ArrayList<Integer>();
 
-    Product(String name, int price, String size, int grade, String category) {
+    Product(String name, int price, String size, int grade, String category, List<Integer> gradeList) {
+        this.gradeList = gradeList;
         this.name = name;
         this.price = price;
         this.size = size;
@@ -18,20 +24,60 @@ public class Product implements ProductInterface {
     }
 
     @Override
-    public int price() {
-        return this.price;
-
+    public void setName(String nameInput) {
+        this.name = nameInput;
     }
 
     @Override
-    public void size() {
-        // generates the size
-
-    }
-
-    @Override
-    public String name() {
+    public String getName() {
         return this.name;
-
     }
+
+    @Override
+    public void setPrice(int newPrice) {
+        this.price = newPrice;
+    }
+
+    @Override
+    public int getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public void setGrade(int gradeInput) {
+        this.gradeList.add(gradeInput);
+        this.grade = (int)Math.round(calcListAverage(gradeList));
+    }
+
+    @Override
+    public int getGrade() {
+        return this.grade;
+    }
+
+    @Override
+    public double calcListAverage(List<Integer> inputList) {
+        int sum = 0;
+        for (int i : inputList){
+            sum += i;
+        }
+        return inputList.size() > 0 ? sum / inputList.size() : 0;
+    }
+
+    // @Override
+    // public int price() {
+    //     return this.price;
+
+    // }
+
+    // @Override
+    // public void size() {
+    //     // generates the size
+
+    // }
+
+    // @Override
+    // public String name() {
+    //     return this.name;
+
+    // }
 }
